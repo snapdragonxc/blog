2b976aa426b04ae71aae883c416fd634c966b655
Mark Mackenzie <snapdragonxc@gmail.com>
2018-03-17 19:43:17 +1100

    added example config.js file

---
+++ example_config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    port: 8080,
+    dbLogin: 'mongodb://localhost:27017/sasha',
+    mail: {
+        clientId: 'xxxxxx',
+        clientSecret: 'xxxxxx',
+        refreshToken: 'xxxxxx',
+        accessToken: 'xxxxxx'
+    },
+    userName: 'admin',
+    password: 'password'
+}
